74ac673274e2d5ec99a5b089301e4cf632035c58
palash <btech10516.19@bitmesra.ac.in>
2022-10-17 08:59:16 +0530

    fix cors error

--- server.js
+++ server.js
@@ -33,5 +33,17 @@ app.get("/auth-endpoint", auth, (request, response) => {
 
 app.listen(port, () => {
   dbConnect();
+  app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
+    );
+    res.setHeader(
+      "Access-Control-Allow-Methods",
+      "GET, POST, PUT, DELETE, PATCH, OPTIONS"
+    );
+    next();
+  });
   console.log(`Server is running on port ${port}`);
 });
